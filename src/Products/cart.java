5869ae317932402ae61a16e86db3e2da54e5880a
Shivam <54373797+shivam0110@users.noreply.github.com>
2020-12-03 11:29:03 +0530

    Add files via upload

---
+++ src/Products/cart.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Products;
+
+/**
+ *
+ * @author Shivam
+ */
+public class cart {
+    public static int n = 0;
+    public static String[] name = new String[12];
+    public static int[] qty = new int[12];
+    public static int total = 0;
+    
+}
